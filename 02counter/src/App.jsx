fdb4507e9ce671cd34de6195c04826f13a05f6a8
Tanmoy-Santra <tanmoysantra911@gmail.com>
2024-04-19 12:05:21 +0530

    Hooks in react

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'//hooks
+
+import './App.css'
+//let counter=5
+
+
+function App() {
+
+ let [counter,setCounter] = useState(15)
+  //useState hooks
+  //change or poprogate inside the UI
+  const addValue = () => {
+    console.log('value added',counter);
+    //counter=counter+1
+    setCounter(counter+1)
+  }
+
+  const removeValue = () =>{
+    setCounter(counter-1)
+  }
+  return (
+    <>
+     <h1>react with vite</h1>
+     <h3>counter value : {counter} </h3>
+     <button onClick={addValue}>Add value</button><br /><br />
+     <button onClick={removeValue}>remove value</button>
+    </>
+  )
+}
+
+export default App
