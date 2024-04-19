b380e47e4294bd395f50703568bf9207fb491076
Tanmoy-Santra <tanmoysantra911@gmail.com>
2024-04-19 10:03:38 +0530

    Day-2

---
+++ my-app/src/Add.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Add = () => {
+  return (
+    <div>
+      <h1>hiiiii</h1>
+    </div>
+  )
+}
+
+export default Add
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Add from './Add';
+
 function App() {
   return (
     <div className="App">
       <h1>first react app</h1>
+      <Add/>
     </div>
   );
 }
